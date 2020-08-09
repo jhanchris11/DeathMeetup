deb684ecf955a51b9ed8bbe184893a3345dc2c41
StartLord11 <jccmunac.97@gmail.com>
2020-08-09 15:43:29 -0500

    Home styled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,12 @@ import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import { Layout } from 'antd';
 import Aside from './components/Layout/Aside';
 import Header from './components/Layout/Header/Header';
-import Main from './pages/Main';
-import Profile from './pages/Profile';
+
 import './App.css';
 import Meet from './pages/Meet/Meet';
-import SignUp from './components/Sign/SignUp';
-import SignIn from './components/Sign/SignIn';
+
 import CategoryState from './context/category/categoryState';
-import Home from './components/Home/Home';
+import Home from './pages/Home/Home';
 
 function App() {
 	return (
@@ -19,15 +17,12 @@ function App() {
 				<Fragment>
 					<Switch>
 						<Route exact path="/" component={Home} />
-						{/* <Route path='/signin' component={SignIn} />
-            <Route path="/signup" component={SignUp} /> */}
 						<Layout className="cl-layout">
 							<Aside />
 							<Layout>
 								<Header />
-								<Route path="/main" component={Main} />
+
 								<Route path="/meet" component={Meet} />
-								<Route path="/profile" component={Profile} />
 							</Layout>
 						</Layout>
 					</Switch>
